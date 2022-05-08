5696cad936f84024f170b76804956a4018ff1dbb
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-05-08 12:35:38 +0700

    implement UI design in activity_main.xml

--- app/src/main/java/com/artworkspace/themovie/MainActivity.kt
+++ app/src/main/java/com/artworkspace/themovie/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.artworkspace.themovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.artworkspace.themovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.mainToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_manu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_search -> {
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menu_favorite -> {
+                Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
